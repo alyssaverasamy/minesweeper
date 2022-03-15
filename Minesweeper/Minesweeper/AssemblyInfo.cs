e4cb015a5f2e9e219e63f7110e86f5e4912c44f7
alyssa <verasamy@shernet.sheridancollege.ca>
2022-03-15 14:20:48 -0400

    Added custom fonts and slight UI tweaks.

--- Minesweeper/Minesweeper/AssemblyInfo.cs
+++ Minesweeper/Minesweeper/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 ﻿using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Fredoka-Regular.ttf", Alias = "Fredoka")]
+[assembly: ExportFont("BalooThambi2-Regular.ttf", Alias = "BalooThambi")] 
