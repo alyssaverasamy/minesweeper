2835fd42219c95b2ef6771313d2f6c6521a13b4f
alyssa <verasamy@shernet.sheridancollege.ca>
2022-03-14 21:36:17 -0400

    created basic pages that link to one another using xamarin navigation

---
+++ Minesweeper/Minesweeper/RulesPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Minesweeper
+{    
+    public partial class RulesPage : ContentPage
+    {    
+        public RulesPage ()
+        {
+            InitializeComponent ();
+        }
+    }
+}
+
---
+++ Minesweeper/Minesweeper/SelectDifficultyPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Minesweeper
+{    
+    public partial class SelectDifficultyPage : ContentPage
+    {    
+        public SelectDifficultyPage ()
+        {
+            InitializeComponent ();
+        }
+    }
+}
+
