2275338ccbc6e2a14dad765f513c41c7aed5081e
Tejas Kumar <kumateja@shernet.sheridancollege.ca>
2022-03-15 22:23:30 -0400

    Added content page and c# logic to generate grid of buttons

--- Minesweeper/Minesweeper/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using Xamarin.Forms.Xaml;
-using Xamarin.Forms;
-
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("Fredoka-Regular.ttf", Alias = "Fredoka")]
-[assembly: ExportFont("BalooThambi2-Regular.ttf", Alias = "BalooThambi")] 
