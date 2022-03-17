1e68961695ba8552f44c39650d17c37854e318fa
alyssa <verasamy@shernet.sheridancollege.ca>
2022-03-17 09:10:02 -0400

    made difficulty page modular by passing data to PlayGame page when difficulty selected.

---
+++ Minesweeper/Minesweeper/PauseMenu.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Minesweeper
+{    
+    public partial class PauseMenu : ContentPage
+    {    
+        public PauseMenu ()
+        {
+            InitializeComponent ();
+        }
+
+        private async void ResumeButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private async void QuitButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+
+        private async void RulesButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RulesPage());
+        }
+    }
+}
+
---
+++ Minesweeper/Minesweeper/PlayGame.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Minesweeper
+{    
+    public partial class PlayGame : ContentPage
+    {
+        int _height;
+        int _width;
+        int _mines;
+
+        public PlayGame (int height, int width, int mines)
+        {
+            InitializeComponent ();
+            _height = height;
+            _width = width;
+            _mines = mines;
+
+            for (int rowI = 0; rowI < _height; rowI++)
+            {
+                mineGrid.RowDefinitions.Add(new RowDefinition());
+                mineGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                for (int colI = 0; colI < _width; colI++)
+                {
+                    Button mineBox = new Button();
+                    mineBox.BackgroundColor = Color.LightGray;
+                    mineGrid.Children.Add(mineBox, rowI, colI);
+                }
+            }
+
+            minesLeft.Text = _mines.ToString();
+        }
+
+        private async void PauseButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PauseMenu());
+        }
+    }
+}
+
