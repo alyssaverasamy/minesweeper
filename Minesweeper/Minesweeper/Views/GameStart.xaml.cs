f01fe5e5fce0877ee04f9f5944afcb57f2be7d9b
Tejas Kumar <kumateja@shernet.sheridancollege.ca>
2022-03-30 19:50:21 -0400

    Added DigCell and FlagCell functions to UI logic. DigCell will open up cell while FlagCell will flag it. While these methods work. The current implementation does not reflect the rule of the game Mine Sweeper. Therefore modifications will need to be made.

--- Minesweeper/Minesweeper/Views/GameStart.xaml.cs
+++ Minesweeper/Minesweeper/Views/GameStart.xaml.cs
@@ -41,7 +41,8 @@ namespace Minesweeper.Views //this is sample page template page, will not be inc
         { 
             foreach(MineButton button in MineGrid.Children)
             {
-                button.Clicked -= StartGame; 
+                button.Clicked -= StartGame;
+                button.Clicked += PlayGame; 
             }
 
             mineGrid.PlaceMines();
@@ -58,6 +59,159 @@ namespace Minesweeper.Views //this is sample page template page, will not be inc
         {
             FlagBtn.IsEnabled = false;
             DigBtn.IsEnabled = true;
+        }
+
+        private void PlayGame(object sender, EventArgs e)
+        {
+
+            if (FlagBtn.IsEnabled)
+                DigCell(sender as MineButton);
+
+            if (DigBtn.IsEnabled)
+                FlagCell(sender as MineButton);
+        }
+
+        private void DigCell(MineButton button)
+        {
+            switch (button._nearbyMines)
+            {
+                case 0:
+                    if (button.isMine)
+                    {
+                        GameOver();
+                    }
+                    else
+                    {
+                        button.Text="";
+                        button.BackgroundColor = Color.White;
+
+                    }
+                    break;
+                case 1:
+                    if (button.isMine)
+                    {
+                        GameOver();
+                    }
+                    else
+                    {
+                        button.Text="1";
+                        button.TextColor = Color.Blue;
+                        button.BackgroundColor = Color.White;
+
+                    }
+                    break;
+                case 2:
+                    if (button.isMine)
+                    {
+                        GameOver();
+                    }
+                    else
+                    {
+                        button.Text="2";
+                        button.TextColor = Color.Green;
+                        button.BackgroundColor = Color.White;
+
+                    }
+                    break;
+                case 3:
+                    if (button.isMine)
+                    {
+                        GameOver();
+                    }
+                    else
+                    {
+                        button.Text="3";
+                        button.TextColor = Color.Red;
+                        button.BackgroundColor = Color.White;
+
+                    }
+                    break;
+                case 4:
+                    if (button.isMine)
+                    {
+                        GameOver();
+                    }
+                    else
+                    {
+                        button.Text="4";
+                        button.TextColor = Color.DarkBlue;
+                        button.BackgroundColor = Color.White;
+
+                    }
+                    break;
+                case 5:
+                    if (button.isMine)
+                    {
+                        GameOver();
+                    }
+                    else
+                    {
+                        button.Text="5";
+                        button.TextColor = Color.DarkGreen;
+                        button.BackgroundColor = Color.White;
+
+                    }
+                    break;
+                case 6:
+                    if (button.isMine)
+                    {
+                        GameOver();
+                    }
+                    else
+                    {
+                        button.Text="6";
+                        button.TextColor = Color.DarkRed;
+                        button.BackgroundColor = Color.White;
+
+                    }
+                    break;
+                case 7:
+                    if (button.isMine)
+                    {
+                        GameOver();
+                    }
+                    else
+                    {
+                        button.Text="7";
+                        button.TextColor = Color.DarkSlateGray;
+                        button.BackgroundColor = Color.White;
+
+                    }
+                    break;
+                case 8:
+                    if (button.isMine)
+                    {
+                        GameOver();
+                    }
+                    else
+                    {
+                        button.Text="8";
+                        button.TextColor= Color.Black;
+                        button.BackgroundColor = Color.White;
+
+                    }
+                    break;
+            }
+        }
+
+
+        private void FlagCell(MineButton button)
+        {
+            button.isFlagged=true;
+            button.Text="~";
+        }
+        private void GameOver()
+        {
+            foreach (MineButton button in MineGrid.Children)
+            {
+                if (button.isMine)
+                {
+                    button.Text="*";
+                    button.TextColor = Color.Red;
+                }
+            }
+
+            DisplayAlert("GameOver", "Sorry", "OK");
         }
     }
 }
