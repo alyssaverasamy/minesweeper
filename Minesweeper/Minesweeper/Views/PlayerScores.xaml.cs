9e58d56157029611436fb47d91aa2abb24e87e4f
Tejas Kumar <kumateja@shernet.sheridancollege.ca>
2022-04-17 19:02:59 -0400

    Added, PlayScores.xaml and cs page. This page will be generated everytime a player wins. New players can enter a NEW id and name and add themselves to the list. To update their scores, they can select their names from the list and click on update to increase their score.

---
+++ Minesweeper/Minesweeper/Views/PlayerScores.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Minesweeper.PlayerLogic;
+using Minesweeper.GameService;
+using System.IO;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.ComponentModel;
+
+
+
+namespace Minesweeper.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlayerScores : ContentPage
+    {
+        PlayerRepository playerRepository = new PlayerRepository();
+        IPlayerManager _playerManager;
+
+        string _filePath = Path.Combine(FileSystem.AppDataDirectory, "players.csv");
+
+        public PlayerScores()
+        {
+            InitializeComponent();
+            _playerManager = new PlayerCSVManager(_filePath);
+            ReadPlayers();
+            LstofPlayers.ItemsSource = playerRepository.Players;
+        }
+
+        private Player CapturePlayerInfo()
+        {
+            int id = int.Parse(EntID.Text);
+            string name = EntName.Text;
+            int firstWin = 1;
+
+            Player newPlayer = new Player(id, name, firstWin);
+
+            return newPlayer;
+        }
+
+        private void AddPLayer_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Player newPlayer = CapturePlayerInfo();
+                playerRepository.AddPlayer(newPlayer);
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Something Went Wrong:", ex.Message, "OK");
+            }
+
+
+        }
+
+        private void UpdatePlayer_Clicked(object sender, EventArgs e)
+        {
+            Player lookUp = LstofPlayers.SelectedItem as Player;
+
+            try
+            {
+                Player foundPlayer = playerRepository.SearchPlayer(lookUp);
+
+                foundPlayer.PlayerWins++;
+            }
+            catch (Exception)
+            {
+                DisplayAlert("Could not find player", "Please re-try", "Re-Try");
+            }
+
+
+        }
+
+        private void SaveAll(object sender, EventArgs e)
+        {
+            try
+            {
+                playerRepository.SaveAllPlayers(_playerManager);
+            }
+            catch (Exception)
+            {
+                DisplayAlert("Writing Error", "Error while writing players, data is not saved", "OK");
+            }
+        }
+
+        private void ReadPlayers()
+        {
+            try
+            {
+                playerRepository.ReadAllPlayers(_playerManager);
+            }
+            catch (Exception)
+            {
+                DisplayAlert("Reading Error", "Cannot Read players", "OK");
+            }
+        }
+    }
+}
