c07fc2f00e372cfe869bb92c398de8870c28d638
Tejas Kumar <kumateja@shernet.sheridancollege.ca>
2022-04-17 18:43:29 -0400

    Added folder called PlayerLogic. Folder contains logic for class Player and  class PlayerRepository, which is a collection of saved players

---
+++ Minesweeper/Minesweeper/PlayerLogic/Player.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Mine.PlayerLogic
+{
+    internal class Player : INotifyPropertyChanged
+    {
+        private int _playerID;
+        private string _playerName;
+        private int _playerWins;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int PlayerID
+        {
+            get { return _playerID; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Player ID cannot be 0 or less than 0");
+                _playerID = value;
+            }
+        }
+
+        public string PlayerName
+        {
+            get { return _playerName; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("Name cannot be empty");
+                _playerName = value;
+            }
+        }
+
+        public int PlayerWins
+        {
+            get { return _playerWins; }
+            set
+            {
+                _playerWins = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PlayerWins)));
+            }
+        }
+
+
+
+
+        public Player(int playerID, string playerName, int playerWins)
+        {
+            PlayerID = playerID;
+            PlayerName = playerName;
+            PlayerWins = playerWins;
+
+        }
+
+
+        public void UpdateWins()
+        {
+            PlayerWins++;
+
+
+        }
+
+
+
+        public override bool Equals(object obj)
+        {
+            Player objPlayer = obj as Player;
+            if (objPlayer == null)
+                return false;
+            return objPlayer.PlayerID == PlayerID;
+        }
+
+        public override int GetHashCode()
+        {
+            return PlayerID.GetHashCode();
+        }
+
+        public static Player Parse(string playerAsString)
+        {
+            Player player = null;
+            string[] parameters = playerAsString.Split(',');
+            int playerID = int.Parse(parameters[0].Trim());
+            string playerName = parameters[1].Trim();
+            int playerWins = int.Parse(parameters[2].Trim());
+            player = new Player(playerID, playerName, playerWins);
+            return player;
+
+        }
+        public override string ToString()
+        {
+            return $"{PlayerID}, {PlayerName}, {PlayerWins}";
+        }
+    }
+}
---
+++ Minesweeper/Minesweeper/PlayerLogic/PlayerRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Mine.AppService;
+
+namespace Mine.PlayerLogic
+{
+    internal class PlayerRepository
+    {
+        private ObservableCollection<Player> _players = new ObservableCollection<Player>();
+
+        public ObservableCollection<Player> Players => _players;
+        public void AddPlayer(Player newPlayer)
+        {
+            foreach (Player player in _players)
+                if (player.PlayerID == newPlayer.PlayerID)
+                    throw new Exception("Duplicate Player IDs, Please Re-enter");
+            _players.Add(newPlayer);
+        }
+
+        public Player SearchPlayer(Player searchedPlayer)
+        {
+            List<Player> searchlist = new List<Player>(Players);
+            Player foundPlayer = searchlist.Find(x => x.PlayerID == searchedPlayer.PlayerID);
+            return foundPlayer;
+
+        }
+
+        public void SaveAllPlayers(IPlayerManager playerManager)
+        {
+            playerManager.SaveAll(Players);
+        }
+        public void ReadAllPlayers(IPlayerManager playerManager)
+        {
+            List<Player> savedList = playerManager.ReadAll();
+            foreach (Player player in savedList)
+            {
+                _players.Add(player);
+            }
+        }
+
+
+    }
+}
